ed839f05dcf10f4b0b2bd5f80074b2bca44d1597
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-03-15 19:46:21 +0300

    Assignment Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// function expression called `divide`
+let num1 = 2000;
+let num2 = 100;
+
+const divide = function() {
+  return num1 / num2;
+};
+
+// arrow function called `square`
+const square = (num) => {
+  return num * num;
+};
+
+// arrow function called `add`
+const add = (num1, num2) => {
+  return num1 + num2;
+};
